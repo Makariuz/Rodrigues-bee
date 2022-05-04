338462458cb99a98164a5754b076f91d721b63c1
Makariuz <samuel.eduardo23@hotmail.com>
2022-05-04 20:04:44 +0100

    view prod details ipad

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -26,7 +26,7 @@ export function Products() {
   const [total, setTotal] = useState(0);
 
   const [productDetails, setProductDetails] = useState([]);
-  const [seeMore, setSeeMore] = useState(true)
+  const [seeMore, setSeeMore] = useState(true);
 
   const [storeImages, setStoreImages] = useState([pic1, pic2, pic3]);
 
@@ -62,8 +62,6 @@ export function Products() {
   };
 
   const addProdDetails = (name, price, image, description) => {
-    
-   
     setProductDetails({ name, price, image, description });
     setEmptyCart(false);
   };
@@ -75,66 +73,71 @@ export function Products() {
 
   return (
     <>
-    {products ? (
-    <div className="products__list__container">
+      {products ? (
+        <div className="products__list__container">
+          <div className="products__list__wrapper">
+            {products.map((product) => {
+              return (
+                <div key={product._id} className="prod__card">
+                  <div className="prod__card__pic">
+                    <img
+                      height="50px"
+                      width="50px"
+                      src={product.image}
+                      alt=""
+                    />
+                  </div>
 
+                  <div className="prod__card__name">
+                    <h3> {product.name}</h3>
+                    <small>
+                      {" "}
+                      <i>See more</i>{" "}
+                    </small>
+                  </div>
 
-      <div className="products__list__wrapper">
-      
-        {products.map((product) => {
-          return (
-            <div key={product._id} className="prod__card">
-              <div className="prod__card__pic">
-                <img height="50px" width="50px" src={product.image} alt="" />
-              </div>
+                  <div className="prod__card__price">
+                    <span> {product.price} €</span>
+                  </div>
+                  <button
+                    onClick={() =>
+                      addProdDetails(
+                        product.name,
+                        product.price,
+                        product.image,
+                        product.description
+                      )
+                    }
+                  >
+                    View <AiOutlineFundView />
+                  </button>
+                  <button
+                    value={`${product.name} ${product.price}€ `}
+                    onClick={addItemToCart}
+                  >
+                    Add <BsCartPlus />{" "}
+                  </button>
+                </div>
+              );
+            })}
+          </div>
 
-              <div className="prod__card__name">
-                <h3> {product.name}</h3>
-                <small>
-                  {" "}
-                  <i>See more</i>{" "}
-                </small>
-              </div>
-
-              <div className="prod__card__price">
-                <span> {product.price} €</span>
-              </div>
-              <button
-                onClick={() =>
-                  addProdDetails(
-                    product.name,
-                    product.price,
-                    product.image,
-                    product.description
-                  )
+          <div
+            className={
+              "products__shop__cart__wrapper " + (!emptyCart && "active")
+            }
+          >
+            <div className="empty__cart">
+              <ProgressiveImage
+                preview={pic1}
+                src={
+                  storeImages[Math.floor(Math.random() * storeImages.length)]
                 }
-              >
-                View <AiOutlineFundView />
-              </button>
-              <button
-                
-                value={`${product.name} ${product.price}€ `}
-                onClick={addItemToCart}
-              >
-                Add <BsCartPlus />{" "}
-              </button>
+                render={(src, style) => <img src={src} style={style} alt="" />}
+              />
             </div>
-          );
-        })}
-      </div>
 
-      <div
-        className={"products__shop__cart__wrapper " + (!emptyCart && "active")}
-      >
-        <div className="empty__cart">
-          <ProgressiveImage
-            preview={pic1}
-            src={storeImages[Math.floor(Math.random() * storeImages.length)]}
-            render={(src, style) => <img src={src} style={style} alt="" />}
-          />
-        </div>
-
-        {/*         {cart &&
+            {/*         {cart &&
           cart.map((item, i) => {
             return (
               <div key={item._id} className="prod__cart__list">
@@ -151,98 +154,92 @@ export function Products() {
             );
           })} */}
 
-        {productDetails && (
-          <div className="prod__detail__wrapper">
-            <div
-              className={"prod__detail__item " + (emptyCart && "hide__details")}
-            >
-              <IoMdCloseCircleOutline
-                className="close__btn"
-                onClick={handleDetails}
-              />
-              <img src={productDetails.image} alt="" />
-              <h3>{productDetails.name}</h3>
-              <p> {productDetails.description} </p>
-              <small>
-                <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf />
-             
-              </small>
-              <small>{productDetails.price} €</small>
-              <button onClick={addItemToCart}>
-                Add to Cart <BsCartPlus />{" "}
-              </button>
-            </div>
+            {productDetails && (
+              <div className="prod__detail__wrapper">
+                <div
+                  className={
+                    "prod__detail__item " + (emptyCart && "hide__details")
+                  }
+                >
+                  <IoMdCloseCircleOutline
+                    className="close__btn"
+                    onClick={handleDetails}
+                  />
+                  <img src={productDetails.image} alt="" />
 
-          </div>
-        )}
-      </div>
+                  <h3>{productDetails.name}</h3>
 
-      <div className="product-wrapper">
-        {/* https://css-tricks.com/css-only-carousel/ */}
-
-        <div className="product-slides">
-          {products.map((product) => {
-            return (
-              <div key={product._id} id="product-slide">
-                
-                <img src={product.image} alt="" className="see-more" />
-                <h3>{product.name} </h3> 
-
-               <p> {product.price} € </p>
-              
-          
-                <button
-               
-                onClick={() =>
-                  addProdDetails(
-                    product.name,
-                    product.price,
-                    product.image,
-                    product.description
-                  )
-                }
-              >
-                View <AiOutlineFundView/>
-              </button>
-           
+                  <p> {productDetails.description} </p>
+                  <small>
+                    <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarFill />
+                    <BsStarHalf />
+                  </small>
+                  <small>{productDetails.price} €</small>
+                  <button onClick={addItemToCart}>
+                    Add to Cart <BsCartPlus />{" "}
+                  </button>
                 </div>
-            );
-          })}
-        </div>
+              </div>
+            )}
+          </div>
 
-        <div className={'product__details '   + (emptyCart && 'empy__details')}>
-     
-            <div className='detail__card'>
-            <IoMdCloseCircleOutline
-                className="close__btn"
-                onClick={handleDetails}
-              />
-           {/*  <img src={productDetails.image} alt="" className={seeMore && 'hideImage'}/> */}
-              <h3>{productDetails.name}</h3>
-              
-              <p> 
-                {productDetails.description} 
-    
-               </p>
-              
-          
-              <small>
-                <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf />
-                <BsStar />
-              </small>
-              <small>{productDetails.price} €</small>
+          <div className="product-wrapper">
+            {/* https://css-tricks.com/css-only-carousel/ */}
 
-              <button  onClick={() => setSeeMore(true)} >
-                Add <BsCartPlus />{" "}
-              </button>
+            <div className="product-slides">
+              {products.map((product) => {
+                return (
+                  <div key={product._id} id="product-slide">
+                    <img src={product.image} alt="" className="see-more" />
+                    <h3>{product.name} </h3>
+
+                    <p> {product.price} € </p>
+
+                    <button
+                      onClick={() =>
+                        addProdDetails(
+                          product.name,
+                          product.price,
+                          product.image,
+                          product.description
+                        )
+                      }
+                    >
+                      View <AiOutlineFundView />
+                    </button>
+                  </div>
+                );
+              })}
             </div>
-             
 
+            <div
+              className={"product__details " + (emptyCart && "empy__details")}
+            >
+              <div className="detail__card">
+                <IoMdCloseCircleOutline
+                  className="close__btn"
+                  onClick={handleDetails}
+                />
+                {/*  <img src={productDetails.image} alt="" className={seeMore && 'hideImage'}/> */}
+                <h3>{productDetails.name}</h3>
+
+                <p>{productDetails.description}</p>
+
+                <small>
+                  <BsStarFill /> <BsStarFill /> <BsStarFill /> <BsStarHalf />
+                  <BsStar />
+                </small>
+                <small>{productDetails.price} €</small>
+
+                <button onClick={() => setSeeMore(true)}>
+                  Add <BsCartPlus />{" "}
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    ) : (
-      <div className="loading__screen">
+      ) : (
+        <div className="loading__screen">
           <div className="img__wrapper__loading">
             <img src="/assets/bees__loading.png" alt="" />
           </div>
@@ -250,7 +247,7 @@ export function Products() {
             <h1>Loading..</h1>
           </div>
         </div>
-    )}
+      )}
     </>
   );
 }
